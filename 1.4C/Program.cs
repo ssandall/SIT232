421fa4c3108a940a32c6a3df406a250df919ec45
Suriya <suriyasandall@gmail.com>
2019-03-18 19:31:34 +1100

    NEW: Task 1.4C

---
+++ 1.4C/Program.cs
@@ -0,0 +1,17 @@
+using System;
+using SplashKitSDK;
+
+namespace Animation
+{
+    public class Program
+        {
+            public static void Main()
+                {
+                    // Variables
+                    Window page1;
+                    Window page2;
+                    Window page3;
+                }
+        }   
+}
+
