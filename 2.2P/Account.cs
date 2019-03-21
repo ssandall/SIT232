a1e881b6e2d7f02ad2dc496914efea2ed34fef41
Suriya <suriyasandall@gmail.com>
2019-03-22 01:41:41 +1100

    NEW/UPDATE: 2.2P Completed Files

---
+++ 2.2P/Account.cs
@@ -0,0 +1,46 @@
+using System;
+
+public class Account{
+
+    private Decimal _balance;
+    private String  _name;
+
+    // Read Only Account Name
+    public string Name{
+
+        get { return _name;}
+
+    }
+
+    // Account Method
+    public Account (string name, decimal startingBalance){
+
+        _name = name;
+        _balance = startingBalance;
+    }
+
+    // Deposit Method
+    public void Deposit(decimal amountToAdd){
+
+        _balance = _balance + amountToAdd;
+
+    }
+
+    // Withdraw Method
+    public void Withdraw(decimal amountToWithdraw){
+
+        _balance = _balance - amountToWithdraw;
+
+    }
+
+    // Print Details to console
+    public void Print(){
+
+        Console.WriteLine("Account Name is : {0}",_name);
+        Console.WriteLine("Account Balance is : {0}",_balance);
+
+    }
+
+    
+
+}
