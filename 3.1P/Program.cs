c367b08bd125b4f16ca92cf06cd477cbd814d5a7
Suriya <suriyasandall@gmail.com>
2019-03-29 01:31:21 +1100

    COMPLETE: Task completed and fully functional

--- 3.1P/Program.cs
+++ 3.1P/Program.cs
@@ -23,7 +23,7 @@ public class Program
                 break;
 
                 case MenuOption.GuessThatNumber:
-                Console.WriteLine("You Have Selected Guess That Number");
+                GuessThatNumber();
                 break;
 
                 case MenuOption.Quit:
@@ -40,7 +40,15 @@ public class Program
         Console.WriteLine("1, will run TestName, 2 will run Guess that number, 3 Wil quit the program.");
         do 
         {
+            try
+            {
             Option = Convert.ToInt32(Console.ReadLine());
+            }
+            catch
+            {
+                Console.WriteLine("Your selection is invalid. Please enter a valid number");
+                Option = -1;
+            }
         } while (Option < 1 || Option > 3);
         return (MenuOption)(Option - 1);
     }
@@ -51,8 +59,64 @@ public class Program
         Console.WriteLine("You Have Selected Test Name");
         Console.Write("Please eneter your name: ");
         userName = Console.ReadLine();
-        Console.WriteLine("Hello "+ userName); 
 
-        
+        if(userName.ToLower() == "suriya")
+        {
+            Console.WriteLine("Welcome my human overlord!");
+        }
+        else if (userName.ToLower() == "jake")
+        {
+            Console.WriteLine("Wassup "+ userName);
+        }
+        else
+        {
+            Console.WriteLine("Hello "+ userName);
+        }
+    }
+
+    private static void GuessThatNumber()
+    {
+        int target;
+        int userGuess;
+        int lowestGuess = 1;
+        int highestGuess = 100;
+        string lowGuess = Convert.ToString(lowestGuess);
+        string highGuess = Convert.ToString(highestGuess);
+
+        target = new Random().Next(100) +1;
+        Console.WriteLine("Guess a number between {0} and {1}",lowGuess,highGuess);
+        do
+        {
+            userGuess = ReadGuess(lowestGuess,highestGuess);
+            if (userGuess < target)
+            {
+                Console.WriteLine("Your Guess is Lower than the answer");
+                lowestGuess = userGuess;
+            }
+            else if(userGuess > target)
+            {
+                Console.WriteLine("Your guess is Higher than the answer");
+                highestGuess = userGuess;
+            }
+            else
+            {
+                Console.Write("You guessed the number!");
+            }
+        } 
+        while (userGuess != target);
+    }
+
+    private static int ReadGuess(int min,int max)
+    {
+        int enteredValue;
+        Console.WriteLine("Enter a guess between {0} and {1}",min,max);
+        do
+        {
+            enteredValue = Convert.ToInt32(Console.ReadLine());
+        } 
+        while (enteredValue < min || enteredValue > max);
+
+        Console.WriteLine("You chose "+ enteredValue);
+        return(enteredValue);
     }
 }
