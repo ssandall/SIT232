e2365f69a060c6bd7bfd1672e1d2b425954392e4
Suriya <suriyasandall@gmail.com>
2019-03-28 23:40:36 +1100

    UPDATE: End Session, TestName pt1 Working

--- 3.1P/Program.cs
+++ 3.1P/Program.cs
@@ -1,10 +1,58 @@
 using System;
 using SplashKitSDK;
 
+public enum MenuOption {
+    TestName,
+    GuessThatNumber,
+    Quit
+}
+
 public class Program
 {
     public static void Main()
     {
+        MenuOption userSelection;
+        do{
+            userSelection = ReadUserOption();
+            // Console.WriteLine("You selected: " + userSelection);
 
+            switch (userSelection) 
+            {
+                case MenuOption.TestName:
+                TestName();
+                break;
+
+                case MenuOption.GuessThatNumber:
+                Console.WriteLine("You Have Selected Guess That Number");
+                break;
+
+                case MenuOption.Quit:
+                Console.WriteLine("You Have Chosen Quit, Thankyou.");
+                break;
+            }
+        }while(userSelection != MenuOption.Quit);
+    }
+
+    private static MenuOption ReadUserOption()
+    {
+        int Option;
+        Console.WriteLine("Please make a selection");
+        Console.WriteLine("1, will run TestName, 2 will run Guess that number, 3 Wil quit the program.");
+        do 
+        {
+            Option = Convert.ToInt32(Console.ReadLine());
+        } while (Option < 1 || Option > 3);
+        return (MenuOption)(Option - 1);
+    }
+
+    private static void TestName()
+    {
+        string userName;
+        Console.WriteLine("You Have Selected Test Name");
+        Console.Write("Please eneter your name: ");
+        userName = Console.ReadLine();
+        Console.WriteLine("Hello "+ userName); 
+
+        
     }
 }
